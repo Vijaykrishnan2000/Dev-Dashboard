4a58079328c5ca1c17667e5bfe389521fbdbb5a8
Vijaykrishnan2000 <67972944+Vijaykrishnan2000@users.noreply.github.com>
2026-03-24 21:39:29 +0530

    Update gemini-proxy.js

--- netlify/functions/gemini-proxy.js
+++ netlify/functions/gemini-proxy.js
@@ -8,7 +8,7 @@ export const handler = async (event) => {
 
   try {
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-flash-lite-preview:generateContent",
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent",
       {
         method: "POST",
         headers: {
